1555cce13d59e9598dc211c132287244d0431364
KIRAN <88279441+KIRAN2023@users.noreply.github.com>
2022-06-07 21:36:36 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+function login() {
+    var registerNo = ['1913032', '1913046', '1913078','19220150'];
+    var password = ['jaga', 'rk', 'sk','rk'];
+    var userName = document.getElementById("name").value;
+    var pass = document.getElementById("pass").value;
+    console.log(`${registerNo}`)
+
+    if ((registerNo.includes(userName)) && (password.includes(pass))) {
+        swal("Good job!", "You Have Successfully logged in", "success"); 
+        if(userName=='1913032'){
+            swal(`Welcome JAGADEESHWARAN`, "Check The Alert to Access The Projects", "success");
+        }else if(userName=='1913046'){
+            swal(`Welcome KIRAN`, "Check The Alert to Access The Projects", "success");
+        }else if(userName=='1913078'){
+            swal(`Welcome SAKTHIVEL`, "Check The Alert to Access The Projects", "success");
+        }else if(userName=='19220150'){
+            swal(`Welcome ROHINI KIRAN`, "Check The Alert to Access The Projects", "success");
+        }    
+        setTimeout(function(){
+            if (window.confirm("Click OK to Access Projects")) {
+                window.open("articlejokes.html");
+            }
+        },3000);
+    }else {
+        swal("Oh No!", "Username/Password incorrect", "error");
+    }
+}
